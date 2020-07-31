3d5f6f198cb3f8068ead086f4658b7218299ec9d
Michael Fursa <bellamy9678@yahoo.com>
2020-07-31 16:14:13 +0300

    feat: add components

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterPanelComponent } from './filter-panel/filter-panel.component';
+import { CardComponent } from './card/card.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterPanelComponent,
+    CardComponent,
+    SearchResultsComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
